9a8140ca8c97fb9e2e37b1258325412de8706e9a
Sagar Ganiga <2015sagar.ganiga@ves.ac.in>
2018-11-15 17:02:07 +0530

    Add m3u8

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const IPFS = require('ipfs')
-const node = new IPFS()
+// const IPFS = require('ipfs')
+// const node = new IPFS()
 
 var fs = require('fs');
 
@@ -10,11 +10,12 @@ var lockFile = require('lockfile');
 var spawn = require('child_process').spawn;
 
 var exec = require('child_process').exec;
+var execSync = require('child_process').execSync;
 
 
-node.on('ready', () => {
-	console.log("IPFS ready")
-})
+// node.on('ready', () => {
+// 	console.log("IPFS ready")
+// })
 
 const WebSocket = require('ws');
 
@@ -45,39 +46,38 @@ wss.on('connection', function connection(ws) {
                 fs.mkdirSync(dir, function(err){
                     if (err) {
                         console.log('failed to create directory', err);
-                    }else{
-
                     }    
                 });
             }
 
-            // console.log(dir);
-            // // Adding to IPFS
-            // let results = node.files.mkdir(dirPath, (err) => {
-            //   if (err) {
-            //     console.error(err)
-            //   }
-            // })
-            // console.log(results);
+            fs.writeFile(dir+"/master.m3u8", "#EXTM3U\n", function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+
+                console.log("The file was saved!");
+            }); 
+
 
             pinCommand = 'ipfs add -Qr '+dir;
 
-            exec(pinCommand, function(error, stdout, stderr) {
-                console.log('stdout: ' + stdout);
-                hash = stdout;
-                // console.log('stderr: ' + stderr);
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                }else{
-
-                    // exec(ipnsCommand+hash, function(error, stdout, stderr) {
-                    //     console.log('stderr: ' + stderr);
-                    //     if (error !== null) {
-                    //         console.log('exec error: ' + error);
-                    //     }
-                    // });
-                }
-            });
+            var hash = execSync(pinCommand);
+            console.log(hash);
+            // exec(pinCommand, function(error, stdout, stderr) {
+            //     console.log('stdout: ' + stdout);
+            //     hash = stdout;
+            //     // console.log('stderr: ' + stderr);
+            //     if (error !== null) {
+            //         console.log('exec error: ' + error);
+            //     }else{
+            //         exec(ipnsCommand+hash, function(error, stdout, stderr) {
+            //             console.log('stderr: ' + stderr);
+            //             if (error !== null) {
+            //                 console.log('exec error: ' + error);
+            //             }
+            //         });
+            //     }
+            // });
 
 
             
@@ -85,7 +85,8 @@ wss.on('connection', function connection(ws) {
             var date = new Date();
 
             // // Name of the master file
-            var name = dir+"/"+date.getTime()+".m3u8";
+            var fileName = date.getTime()+".m3u8";
+            var name = dir+"/"+fileName;
             
             // Args for HLS conversion
 
@@ -115,6 +116,68 @@ wss.on('connection', function connection(ws) {
             proc.on('close', (code, signal) => {
                 console.log('FFmpeg child process closed, code ' + code + ', signal ' + signal);
                 
+                lockFile.lock(dir+"/master.m3u8.lock", function(er){
+                    var wstream = fs.createWriteStream(dir+"/master.m3u8", {'flags': 'a'});
+                    wstream.write("#EXT-X-STREAM-INF:BANDWIDTH=150000\n"+fileName+"\n");
+                    wstream.end();
+
+                    
+
+                    wstream.on('finish', function(){
+                        execSync(pinCommand, function(error, stdout, stderr) {
+                            console.log('stdout: ' + stdout);
+                            hash = stdout;
+                            console.log('stderr: ' + stderr);
+                            if (error !== null) {
+                                console.log('exec error: ' + error);
+                            }else{
+                                // ipfs name publish /ipfs/<CURRENT_PARENTFOLDER_HASH>
+                                
+                                     
+                            }
+                        });    
+                        lockFile.unlock(dir+"/master.m3u8.lock", function(err){
+                            // unlocked;
+                        })
+                    })
+                     
+                });
+
+
+                // var lsCommand = "find "+dir+" -name \"*.m3u8\"  -printf \"%f\n\" | sort"; 
+
+                // var res = execSync(lsCommand).toString();
+                // // console.log(result.toString());
+
+                // var m3u8s = res.split("\n");
+
+                // // console.log(lsCommand);
+
+                // // // lockFile.lock(dir+"/master.m3u8.lock", function (er) {
+                // var catCommand = "cat temp.m3u8 > "+dir+"/master.m3u8";
+                // var file = execSync(catCommand).toString();
+
+                // // // "#EXTM3U\n#EXT-X-STREAM-INF:BANDWIDTH=150000\n"
+
+                // var wstream = fs.createWriteStream(dir+"/master.m3u8", {'flags': 'a'});
+
+                // m3u8s.forEach((m3u8, index) => {
+                //     // console.log(m3u8.split("/"));
+                //     if(m3u8!="master.m3u8" && m3u8 != "")
+                //         wstream.write("#EXT-X-STREAM-INF:BANDWIDTH=150000\n"+m3u8+"\n");
+                    
+                // });
+
+                // wstream.end();
+
+                // wstream.on("finish", function() {
+                    // console.log("finished");
+                    // Writing to the file is actually complete.
+                    // lockFile.unlock(dir+"/master.m3u8.lock", function (er) {
+                    //     // er means that an error happened, and is probably bad.
+                    // })  
+                // });
+
                 // exec(pinCommand, function(error, stdout, stderr) {
                 //     console.log('stdout: ' + stdout);
                 //     hash = stdout;
@@ -147,10 +210,7 @@ wss.on('connection', function connection(ws) {
             //         return console.log(err);
             //     }
             //     console.log("The file was saved!");
-            // });
-
-            
+            // });   
         }
-
     });
 });
