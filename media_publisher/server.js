442cf09d5f1349130c7519a26d735808b36a4f04
SagarGaniga <2015sagar.ganiga@ves.ac.in>
2018-11-28 12:39:09 +0530

    Add comments and move to folders

--- media_publisher/server.js
+++ media_publisher/server.js
@@ -1,53 +1,67 @@
 'use strict';
 
+// IPFS API
 const IPFS = require('ipfs')
+
+// Create IPFS node with pubsub = true
 const node = new IPFS({
     EXPERIMENTAL: {
     pubsub: true
   }
 });
 
+// Websocket for recieving video chunks
+const WebSocket = require('ws');
+
+// Create a socket 
+const wss = new WebSocket.Server({ port: 8000 });
+
+// Filesystem
 var fs = require('fs');
 
+// Lockfile library to lock master.m3u8
 var lockFile = require('lockfile');
 
+// Child Pocess Creation
 var spawn = require('child_process').spawn;
-
 var exec = require('child_process').exec;
 var execSync = require('child_process').execSync;
 
 
+// once the server is ready
 node.on('ready', () => {
-	console.log("IPFS ready", node.id)
+    console.log("IPFS ready", node.id)
 })
 
-const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8000 });
+// Variable to hold the name and directory structure
+// dir holds the path for temp_folder
+// hash holds the hash of newly added content
+// pinCommand holdes the string for adding content to ipfs in child node
+let dir, hash, pinCommand;
 
-
- // Variable to hold the name and directory structure
-let dir, fd, ipns, hash, pinCommand;
-let ipnsCommand = "ipfs name publish /ipfs/"
-
+// Topic for the pubsub to publish the new hash
 const topic = "newHash";
 
 
 // Socket Connection
 wss.on('connection', function connection(ws) {
-
+    // On receiving the message
     ws.on('message', function incoming(message) {
         // Make  an empty folder and add it to ipfs, create the ipns for it
         if(message == "init"){
             
-            var date = new Date();
+            // Current date to name the file uniquely
+            var dateNow = new Date();
             
-            var timeShot = date.getTime();
+            var timeShot = dateNow.getTime();
 
             // Create a directory with this name
             dir = './temp_folder/tmp' + timeShot
             // var dirPath = '/temp_folder/tmp' + timeShot
             
+
+            // Create the directory using filesystem
             if (!fs.existsSync(dir)){
                 fs.mkdirSync(dir, function(err){
                     if (err) {
@@ -56,15 +70,20 @@ wss.on('connection', function connection(ws) {
                 });
             }
 
+            // Add a master.m3u8 file, empty
             fs.writeFile(dir+"/master.m3u8", "#EXTM3U\n", function(err) {
                 if(err) {
                     return console.log(err);
                 }
 
-                console.log("The file was saved!");
-		          pinCommand = 'ipfs add -Qr '+dir;
 
-    	        var hash = execSync(pinCommand);
+                console.log("The file was saved!");
+
+                pinCommand = 'ipfs add -Qr '+dir;
+
+    	       // Add this folder to IPFS
+                var addEmptyFolder = execSync(pinCommand);
+
     	        // console.log(hash.toString);
                 // console.log(node.pubsub);
 
@@ -79,6 +98,8 @@ wss.on('connection', function connection(ws) {
                 //   console.log(`published to ${topic}`)
                 // })
 
+
+                // Publish the hash to pubsub topic "newHash"
                 node.pubsub.publish(topic, new node.types.Buffer('banana'));
 
         		// node.pubsub.publish(topic, hash, (err) =>{
@@ -90,18 +111,16 @@ wss.on('connection', function connection(ws) {
                 
     	    });             
         }else{
-            console.log("In here");
+            // Current date to name the file uniquely
+            var dateNow = new Date();
 
-            var date = new Date();
-
-            // // Name of the master file
-            var fileName = date.getTime()+".m3u8";
-            var name = dir+"/"+fileName;
+            // Name of the master file
+            var fileName = dateNow.getTime()+".m3u8";
+            var finalName = dir+"/"+fileName;
             
             console.log("Name it ", fileName);
 
             // Args for HLS conversion
-
             var args = [
                 '-y', 
                 '-i', '-',                  // - because we are sending in the input
@@ -111,59 +130,50 @@ wss.on('connection', function connection(ws) {
                 '-start_number', '0',     // start the first .ts segment at index 0
                 '-hls_time', '10',        // 10 second segment duration
                 '-hls_list_size', '0',    // Maxmimum number of playlist entries (0 means all entries/infinite)
-                '-f', 'hls', name               // HLS format    
+                '-f', 'hls', finalName               // HLS format    
             ];
 
-            console.log("Arguments ready");
             // Spawn the child process with ffmpeg and args
             const proc = spawn('ffmpeg', args);
 
             console.log("Spawn");
 
             // Write the incoming chunk to the proc
-            console.log("Write the incoming chunk to the proc");
             proc.stdin.write(message);
 
             // End the writing stream
             proc.stdin.end();
-            console.log("End the writing stream");
 
             // If FFmpeg stops for any reason, close the child_process.
             proc.on('close', (code, signal) => {
                 console.log('FFmpeg child process closed, code ' + code + ', signal ' + signal);
                 
 
+                // lock the master
                 lockFile.lock(dir+"/master.m3u8.lock", function(er){
-                    
-                    console.log("lock the master");
-                    
-
                     // Write the filename name to master.m3u8 
-
-
                     console.log("Write the filename name to master.m3u8 ");
+
                     var wstream = fs.createWriteStream(dir+"/master.m3u8", {'flags': 'a'});
+                    
                     wstream.write("#EXT-X-STREAM-INF:BANDWIDTH=150000\n"+fileName+"\n");
                     
                     console.log("End writing");
                     wstream.end();
 
                     
-
-
                     // On finish writing stream
                     wstream.on('finish', function(){
-                        console.log("It is finished");
                         // Add this folder to ipfs
                         try{
                             console.log("Add to IPFS");
-                            var add = execSync(pinCommand);                            
+                            var addNewContent = execSync(pinCommand);                            
                             
-                            console.log("Added");
-                            hash = add;
+                            hash = addNewContent;
                                                             
                             console.log("Send this hash: "+hash);
-                                                            
+                                         
+                            // publish to topic                   
                             console.log("publish to topic");
                             // node.pubsub.publish(topic, hash, (err) =>{
                             //     if(err){
